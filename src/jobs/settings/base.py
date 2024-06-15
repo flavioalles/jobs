21e0fce32e5881d69ea04c844a317bee2894d403
Flavio Alles <flavio.alles@proton.me>
2024-06-15 18:02:39 -0300

    Adds docs to Settings class.

--- src/jobs/settings/base.py
+++ src/jobs/settings/base.py
@@ -4,6 +4,17 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    """
+    Configuration settings for the jobs API.
+
+    Attributes:
+        app_name (str): The name of the application.
+        debug (bool): Flag indicating whether debug mode is enabled.
+        description (str): A description of the API service.
+        database_url (str): The URL of the database.
+
+    """
+
     app_name: str = "jobs"
     debug: bool = False
     description: str = "An API providing a service that manages job postings."
